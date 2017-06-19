2903183a341e73f005c7555ad67c4c2dde0260d9
Russell West <russ.west@live.com>
2017-06-19 11:43:12 -0700

    Add comments/spacing to Update-Link and improve Theme argument validation

--- Update-Link.ps1
+++ Update-Link.ps1
@@ -1,6 +1,16 @@
-param([Parameter(Mandatory=$True)][ValidateScript({Test-Path $_})][string]$Path, [string]$Theme)
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_})]
+    [string]$Path,
+
+    [Parameter()]
+    [ValidateSet('Light','Dark')]
+    [string]$Theme
+)
 
 $lnk = & ("$PSScriptRoot\Get-Link.ps1") $Path
+
+# Set Common Solarized Colors
 $lnk.ConsoleColors[0]="#002b36"
 $lnk.ConsoleColors[8]="#073642"
 $lnk.ConsoleColors[2]="#586e75"
@@ -17,7 +27,8 @@ $lnk.ConsoleColors[5]="#6c71c4"
 $lnk.ConsoleColors[9]="#268bd2"
 $lnk.ConsoleColors[11]="#2aa198"
 $lnk.ConsoleColors[10]="#859900"
-# This defines dark, swap Screen and PopUp for light
+
+# Set Light/Dark Theme-Specific Colors
 if ($Theme -eq "Dark") {
     $lnk.PopUpBackgroundColor=0xf
     $lnk.PopUpTextColor=0x6
@@ -29,5 +40,7 @@ if ($Theme -eq "Dark") {
     $lnk.ScreenBackgroundColor=0xf
     $lnk.ScreenTextColor=0x6
 }
+
 $lnk.Save()
-Write-Host "Updated $Path"
+
+Write-Host "Updated $Path to Solarized - $Theme"
