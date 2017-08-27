b07d13fcb443ac3358083c07f27758f320b9304e
Bevan Arps <bevan.arps@microsoft.com>
2017-08-28 11:45:53 +1200

    Add parameter support

--- Update-StartMenu.ps1
+++ Update-StartMenu.ps1
@@ -1,3 +1,8 @@
+param(
+    [Parameter()]
+    [ValidateSet('Light','Dark')]
+    [string]$theme = 'Dark'
+)
 
 Write-Host "Updating PowerShell shortcuts on the Start Menu"
 
@@ -11,7 +16,7 @@ Write-Host
 $powerShellx86 = "$powerShellFolder\Windows PowerShell (x86).lnk"
 if (test-path $powerShellx86) {
     Write-Host "Updating $powerShellx86"
-    .\Update-Link.ps1 $powerShellx86
+    .\Update-Link.ps1 $powerShellx86 $theme
 } else {
     Write-Warning "Didn't find $powerShellx86"
 }
@@ -20,7 +25,7 @@ Write-Host
 $powerShell64 = "$powerShellFolder\Windows PowerShell.lnk" 
 if (test-path $powerShell64) {
     Write-Host "Updating $powerShell64"
-    .\Update-Link.ps1 $powerShell64
+    .\Update-Link.ps1 $powerShell64 $theme
 } else {
     Write-Warning "Didn't find $powerShell64"
 }
