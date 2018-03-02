5a6fd1c71690e48f2a8aa688b76da6a62bee4da2
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-03-02 15:04:42 -0800

    Described color mappings
    This also changed the pop-up background and text colors to the
    secondary colors of Solarized.

--- Update-Link.ps1
+++ Update-Link.ps1
@@ -11,34 +11,38 @@ param(
 $lnk = & ("$PSScriptRoot\Get-Link.ps1") $Path
 
 # Set Common Solarized Colors
-$lnk.ConsoleColors[0]="#002b36"
-$lnk.ConsoleColors[8]="#073642"
-$lnk.ConsoleColors[2]="#586e75"
-$lnk.ConsoleColors[6]="#657b83"
-$lnk.ConsoleColors[1]="#839496"
-$lnk.ConsoleColors[3]="#93a1a1"
-$lnk.ConsoleColors[7]="#eee8d5"
-$lnk.ConsoleColors[15]="#fdf6e3"
-$lnk.ConsoleColors[14]="#b58900"
-$lnk.ConsoleColors[4]="#cb4b16"
-$lnk.ConsoleColors[12]="#dc322f"
-$lnk.ConsoleColors[13]="#d33682"
-$lnk.ConsoleColors[5]="#6c71c4"
-$lnk.ConsoleColors[9]="#268bd2"
-$lnk.ConsoleColors[11]="#2aa198"
-$lnk.ConsoleColors[10]="#859900"
+# Solarized Colors: http://ethanschoonover.com/solarized
+# NOTE: These are not the same as for the command prompt.
+#                                  | Solarized | PowerShell  |
+#----------------------------------|-----------|-------------|
+$lnk.ConsoleColors[00] = "#002b36" # base03    | Black       |
+$lnk.ConsoleColors[08] = "#073642" # base02    | DarkGray    |
+$lnk.ConsoleColors[02] = "#586e75" # base01    | DarkGreen   |
+$lnk.ConsoleColors[06] = "#657b83" # base00    | DarkYellow  |
+$lnk.ConsoleColors[01] = "#839496" # base0     | DarkBlue    |
+$lnk.ConsoleColors[03] = "#93a1a1" # base1     | DarkCyan    |
+$lnk.ConsoleColors[07] = "#eee8d5" # base2     | Gray        |
+$lnk.ConsoleColors[15] = "#fdf6e3" # base3     | White       |
+$lnk.ConsoleColors[14] = "#b58900" # yellow    | Yellow      |
+$lnk.ConsoleColors[04] = "#cb4b16" # orange    | DarkRed     |
+$lnk.ConsoleColors[12] = "#dc322f" # red       | Red         |
+$lnk.ConsoleColors[13] = "#d33682" # magenta   | Magenta     |
+$lnk.ConsoleColors[05] = "#6c71c4" # violet    | DarkMagenta |
+$lnk.ConsoleColors[09] = "#268bd2" # blue      | Blue        |
+$lnk.ConsoleColors[11] = "#2aa198" # cyan      | Cyan        |
+$lnk.ConsoleColors[10] = "#859900" # green     | Green       |
 
 # Set Light/Dark Theme-Specific Colors
 if ($Theme -eq "Dark") {
-    $lnk.PopUpBackgroundColor=0xf
-    $lnk.PopUpTextColor=0x6
-    $lnk.ScreenBackgroundColor=0x0
-    $lnk.ScreenTextColor=0x1
+    $lnk.ScreenBackgroundColor=0  # base03
+    $lnk.ScreenTextColor=1        # base0
+    $lnk.PopUpBackgroundColor=8   # base02
+    $lnk.PopUpTextColor=3         # base1
 } else {
-    $lnk.PopUpBackgroundColor=0x0
-    $lnk.PopUpTextColor=0x1
-    $lnk.ScreenBackgroundColor=0xf
-    $lnk.ScreenTextColor=0x6
+    $lnk.ScreenBackgroundColor=15 # base3
+    $lnk.ScreenTextColor=1        # base0
+    $lnk.PopUpBackgroundColor=7   # base2
+    $lnk.PopUpTextColor=3         # base1
 }
 
 $lnk.Save()
