e0d3209807c36812a17576fdf5cacbb7573c9949
Neill Robson <neill@neillrobson.com>
2019-01-07 12:48:12 -0500

    Update light PSReadLine configuration
    See the GitHub issue at https://github.com/lzybkr/PSReadLine/issues/738 for details.

--- Set-SolarizedLightColorDefaults.ps1
+++ Set-SolarizedLightColorDefaults.ps1
@@ -14,35 +14,19 @@ $Host.PrivateData.ProgressBackgroundColor = 'Cyan'
 
 # Check for PSReadline
 if (Get-Module -ListAvailable -Name "PSReadline") {
-    $options = Get-PSReadlineOption
-
-    # Foreground
-    $options.CommandForegroundColor = 'Yellow'
-    $options.ContinuationPromptForegroundColor = 'DarkYellow'
-    $options.DefaultTokenForegroundColor = 'DarkYellow'
-    $options.EmphasisForegroundColor = 'Cyan'
-    $options.ErrorForegroundColor = 'Red'
-    $options.KeywordForegroundColor = 'Green'
-    $options.MemberForegroundColor = 'DarkGreen'
-    $options.NumberForegroundColor = 'DarkGreen'
-    $options.OperatorForegroundColor = 'DarkCyan'
-    $options.ParameterForegroundColor = 'DarkCyan'
-    $options.StringForegroundColor = 'Blue'
-    $options.TypeForegroundColor = 'DarkBlue'
-    $options.VariableForegroundColor = 'Green'
-
-    # Background
-    $options.CommandBackgroundColor = 'White'
-    $options.ContinuationPromptBackgroundColor = 'White'
-    $options.DefaultTokenBackgroundColor = 'White'
-    $options.EmphasisBackgroundColor = 'White'
-    $options.ErrorBackgroundColor = 'White'
-    $options.KeywordBackgroundColor = 'White'
-    $options.MemberBackgroundColor = 'White'
-    $options.NumberBackgroundColor = 'White'
-    $options.OperatorBackgroundColor = 'White'
-    $options.ParameterBackgroundColor = 'White'
-    $options.StringBackgroundColor = 'White'
-    $options.TypeBackgroundColor = 'White'
-    $options.VariableBackgroundColor = 'White'
+    Set-PSReadLineOption -Colors @{
+        "Command"            = [ConsoleColor]::Yellow
+        "ContinuationPrompt" = [ConsoleColor]::DarkYellow
+        "DefaultToken"       = [ConsoleColor]::DarkYellow
+        "Emphasis"           = [ConsoleColor]::Cyan
+        "Error"              = [ConsoleColor]::Red
+        "Keyword"            = [ConsoleColor]::Green
+        "Member"             = [ConsoleColor]::DarkGreen
+        "Number"             = [ConsoleColor]::DarkGreen
+        "Operator"           = [ConsoleColor]::DarkCyan
+        "Parameter"          = [ConsoleColor]::DarkCyan
+        "String"             = [ConsoleColor]::Blue
+        "Type"               = [ConsoleColor]::DarkBlue
+        "Variable"           = [ConsoleColor]::Green
+    }
 }
